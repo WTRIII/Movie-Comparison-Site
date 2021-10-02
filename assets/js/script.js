41d4c9282019e3e88e8e67798d9c4f94579cc0fc
Xevshak <keshavsternkrishnan@gmail.com>
2021-10-02 13:31:10 -0400

    added commented out functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,51 @@
-/*function getApi(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      console.log(response.status);
-      //  Conditional for the the response.status.
-      if (response.status !== 200) {
-        // Place the response.status on the page.
-        responseText.textContent = response.status;
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      // Make sure to look at the response in the console and read how 404 response is structured.
-      console.log(data);
-    });
-}
-
-getApi(badRequestUrl);
-*/
+/*var formSubmitHandler = function (event) {
+    event.preventDefault();
+//   changed to match input element
+    var username = InputEl.value;
+    console.log(username)
+  
+    if (username) {
+      getUserRepos(username);
+//   changed to results
+      resultsContainerEl.textContent = '';
+      InputEl.value = '';
+    } else {
+      alert('Please enter a GitHub username');
+    }
+  };
+  
+  var buttonClickHandler = function (event) {
+    // `event.target` is a reference to the DOM element of what programming language button was clicked on the page
+    var language = event.target.getAttribute('data-language');
+  
+    // If there is no language read from the button, don't attempt to fetch repos
+    if (language) {
+      getFeaturedRepos(language);
+//   change to match variable
+      resultsContainerEl.textContent = '';
+    }
+  };
+  
+  var getUserRepos = function (user) {
+    var apiUrl = 'https://www.loc.gov/search/?q=' + user + '&fo=json';
+  
+    fetch(apiUrl)
+      .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data.results[0].date);
+            displayRepos(data, user);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+      .catch(function (error) {
+        alert('Unable to connect to GitHub');
+      });
+  };
+  */
 /* get api url as an input for function
 make function that calls api and checks status
 if status returns error, display error message
