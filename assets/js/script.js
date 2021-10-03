0a858437408ba698bf12846b233d5b8f4d74777e
Xevshak <50416274+Xevshak@users.noreply.github.com>
2021-10-03 17:36:20 -0400

    Updated script with functions to create URLs
    Added functions that keyword search amazon, alibaba, and best buy and then take the results and convert them into an array of URLs that can be read by the product information api function to give us info to compare.

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,3 +55,61 @@ return object out of function
 
 /* create url for search terms, varUrl = 'placeholder format' + search input var + 'last bit placeholder'
 */
+// Var brand will change the url for whichever site we want to search for keyword.  Amazon is '/amz/amazon', Alibaba is '/alb/alibaba', walmart is '/wlm/walmart'
+var getProduct = function (searchEl, brand) {
+  //Either specify brand as part of argument or iterate thru all 3 every time
+var apiUrl = 'http://api-prd.axesso.de' + brand + '-search-by-keyword-asin?keyword=' + searchEl + '&asin='/*api key*/'domainCode=com&sortBy=review-rank' //sort by average user review with review-rank
+fetch(apiUrl)
+  .then(function (response){
+    if (response.ok)  {
+      //Show response in console
+      console.log(response);
+      //To-do: find how response data is structured to isolate info needed
+      response.json().then(function(data){
+        console.log(data);
+        //data is a list of the back end of URLs for the different shoppin sites.  Walmart needs to have 'https://www.walmart.com/' before the tag, unsure for amazon and alibaba rn
+        //I could either keep these if statements in this function, or make a separate one to parse data into URLs to feed the 2nd part of the api
+       if (brand = '/wlm/walmart') { //If the brand is walmart, creates the urls needed to find product info
+         urlArray = [];
+        for (let i = 0; i < data.length; i++) {
+          prodUrl = 'https://www.walmart.com/' + data[i];
+          urlArray.push(prodUrl);
+        }
+        return urlArray
+       };
+
+       if (brand = '/amz/amazon') { //If the brand is amazon, creates the urls needed to find product info
+        urlArray = [];
+       for (let i = 0; i < data.length; i++) {
+         prodUrl = 'https://www.amazon.com/dp/' + data[i];
+         urlArray.push(prodUrl);
+       }
+       return urlArray
+      };
+
+      if (brand = '/alb/alibaba') { //If the brand is alibaba, creates the urls needed to find product info
+        urlArray = [];
+       for (let i = 0; i < data.length; i++) {
+         prodUrl =  data[i];
+         urlArray.push(prodUrl);
+       }
+       return urlArray
+      };
+
+      });
+    } else {
+      //Report errors
+      alert('Error: ' + response.statusText)
+      
+    }
+      })
+      .catch(function (error) {
+        //If the normal error response fails, this will also report an error
+        alert('Unable to connect');
+      });
+    };
+
+// Turn array of URLs into data needed to compare products.  Could do this inside the for loops for each url instead of outputting array but would have to store a lot of data
+/*var getProductInfo = function (prodUrl) {
+  
+}*/
