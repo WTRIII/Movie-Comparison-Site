6447d41b996ff0ece43048cdbfd011a0f11e2da2
Xevshak <keshavsternkrishnan@gmail.com>
2021-10-04 11:41:02 -0400

    added test file for obj

---
+++ assets/js/test.js
@@ -0,0 +1,9 @@
+let prodInfo = {};
+prodInfo.name = "USB PXN-0082 Arcade Joystick Wired Arcade Gaming Controller for Nintendo Switch PC Flight Games Joystick";
+prodInfo.rating = "5 stars";
+prodInfo.price = 19.99;
+prodInfo.image =  [
+    "//sc02.alicdn.com/kf/U082481c3a9234aae900ae945bb6efe99v/USB-PXN-0082-Arcade-Joystick-Wired-Arcade.jpg",
+    "//sc01.alicdn.com/kf/HTB1SRiYjOCYBuNkSnaVq6AMsVXaK/USB-PXN-0082-Arcade-Joystick-Wired-Arcade.jpg"
+]
+console.log(prodInfo)
