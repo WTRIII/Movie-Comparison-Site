b65b06574318afa84f039bc83324b86101b123a4
Xevshak <keshavsternkrishnan@gmail.com>
2021-10-04 15:04:29 -0400

    api function troubleshot and running

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -1,17 +1,99 @@
 // function for search button
 var searchButton = document.querySelector('#search-button');
+/////
+var getProduct = function (searchEl) {
+   console.log(searchEl)
+   searchEl = searchEl.trim()
+    searchEl = searchEl.split(' ').join('%20');
+    fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?s=" + searchEl + "&r=json&page=1", {
+      "method": "GET",
+      "headers": {
+          "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+          "x-rapidapi-key": "69e5639567msh04fcedaeb274ddap1281c2jsnb13aa2fe71aa"
+      }
+  })
+      .then(function (response){
+        console.log(response)
+        if (response.ok)  {
+          //Show response in console
+          console.log(response);
+         
+          response.json().then(function(data){
+            if (data.Response !== 'False') {
+              console.log(data);
+              console.log(data.Search);
+              console.log(getInfo(data.Search[0].imdbID));
+              return getInfo(data.Search[0].imdbID);
+            }
+            if (data.Response == 'False') {
+              console.log('That movie does not exist')
+            }
+  
+          });
+        } else {
+          //Report errors
+          alert('Error: ' + response.statusText);
+        
+        }
+          })
+          .catch(function (error) {
+          //If the normal error response fails, this will also report an error
+            console.log(error); //changed alert to console log
+          });
+        };
+  
+  var getInfo = function (prodUrlID) {
+    fetch("https://movie-database-imdb-alternative.p.rapidapi.com/?r=json&i="+ prodUrlID, {
+      "method": "GET",
+      "headers": {
+          "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+          "x-rapidapi-key": "69e5639567msh04fcedaeb274ddap1281c2jsnb13aa2fe71aa"
+      }
+  })
+      .then(function(response){
+        if (response.ok)  {
+          console.log(response)
+          response.json().then(function(data) {
+            console.log(data);
+            let info = {};
+            info.name = data.Title;
+            info.rating = data.Ratings;
+            info.plot = data.Plot;
+            info.image = data.Poster;
+            console.log(info);
+            return info;
+            });
+          } else {
+            //Report errors
+            alert('Error:'  + response.statusText)
+          }
+        });
+    }
+  // .catch(function (error) {
+  //   //If the normal error response fails, this will also report an error
+  //   alert('Unable to connect');
+  // }
+  
+  
 
+
+
+
+
+/////
 function conductSearch(event){
 event.preventDefault();
+console.log('search function called')
 var searchInputValue = document.querySelector('#search-input');
-searchApi(searchInputValue);
+console.log(searchInputValue.value)
+getProduct(searchInputValue.value);
 if(!searchInputValue){
     console.error('You need a search input value.');
     return;
 }
-var searchString = './search-results.html?q=' + searchInputValue;
+//var searchString = '/display-results.html?q=' + searchInputValue;
 
-location.assign(searchString);
+//location.assign(searchString);
 }
 
-searchButton.addEventListener('submit', conductSearch());
+searchButton.addEventListener('click', conductSearch);
